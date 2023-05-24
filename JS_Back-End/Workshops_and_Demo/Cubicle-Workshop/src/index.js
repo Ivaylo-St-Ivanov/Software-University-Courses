802b9c60289785bd4cc8c188caa5824589690010
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-05-24 09:14:41 +0300

    Add config for express and handlebars.

---
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+
+const homeController = require('./controllers/homeController');
+
+const PORT = 3000;
+
+const app = express();
+
+expressConfig(app);
+handlebarsConfig(app);
+
+app.use(homeController);
+
+app.listen(PORT, () => console.log(`This server is running on port ${PORT}...`));
