960656194d6281eac77a6232fd40a134db0e6192
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-03 11:59:10 +0300

    Add create accessory page.

--- JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/routes.js
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const cubeController = require('./controllers/cubeController');
 const createController =require('./controllers/createController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use(cubeController);
 router.use(createController);
+router.use('/accessories', accessoryController);
 router.use('*', (req, res) => {
     res.render('404');
 });
