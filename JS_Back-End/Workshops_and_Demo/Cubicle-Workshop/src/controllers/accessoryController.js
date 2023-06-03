651dfa09d45797ed88ab92b08de726f92aa39132
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-03 13:02:26 +0300

    Add accessory service and action for create accessory.

--- JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/controllers/accessoryController.js
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/controllers/accessoryController.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
+const accessoryService = require('../services/accessoryService');
+
 router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
-router.post('/create', (req, res) => {
-    console.log(req.body);
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    await accessoryService.create({ name, description, imageUrl });
 
     res.redirect('/');
 });
