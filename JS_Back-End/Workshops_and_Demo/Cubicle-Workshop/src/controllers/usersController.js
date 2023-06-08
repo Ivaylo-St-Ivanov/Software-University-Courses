a308a76edeed224a8c10d4f802853c835b4d04e8
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-08 19:30:15 +0300

    Add user service and user register.

--- JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/controllers/usersController.js
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/controllers/usersController.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+const usersService = require('../services/usersService');
+
 router.get('/register', (req, res) => {
     res.render('users/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    await usersService.register({ username, password, repeatPassword });
+
+    res.redirect('/users/login');
 });
 
 module.exports = router;
