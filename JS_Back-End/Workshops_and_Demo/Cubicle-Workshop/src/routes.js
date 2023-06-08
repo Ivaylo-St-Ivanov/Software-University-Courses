2ea09e5e5334830eeba8b9baf0760524b3e65616
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-08 13:42:35 +0300

    Add users controller and register page.

--- JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/routes.js
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const createController =require('./controllers/createController');
 const accessoryController = require('./controllers/accessoryController');
+const usersController = require('./controllers/usersController');
 
 router.use(cubeController);
 router.use(createController);
 router.use('/accessories', accessoryController);
+router.use('/users', usersController);
 router.use('*', (req, res) => {
     res.render('404');
 });
