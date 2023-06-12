4bd4e7de3e9edd69ed5c0c0e2e01fcdc6ecdde66
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-12 12:39:08 +0300

    Add username and password validation.

--- JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/models/User.js
+++ JS_Back-End/Workshops_and_Demo/Cubicle-Workshop/src/models/User.js
@@ -3,8 +3,24 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        minLength: [5, 'Username is too short!'],
+        match: /^[A-Za-z0-9]+$/,
+        unique: true 
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: 'Invalid password characters!'
+        },
+        minLength: [8, 'Password is too short!']
+    }
 });
 
 userSchema.virtual('repeatPassword')
