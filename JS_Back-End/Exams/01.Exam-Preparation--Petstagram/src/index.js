580a8bec76595c9d85f2e7b0024098b90cb24160
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-13 15:14:54 +0300

    Install mongoose and connect database.

--- JS_Back-End/Exams/01.Exam-Preparation--Petstagram/src/index.js
+++ JS_Back-End/Exams/01.Exam-Preparation--Petstagram/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
+
 const routes = require('./routes');
 
 const PORT = 3000;
@@ -10,6 +12,9 @@ const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
+dbConnect()
+    .then(() => console.log('Database connect successfully.'))
+    .catch(err => console.log('Database error', err));
 
 app.use(routes);
 
