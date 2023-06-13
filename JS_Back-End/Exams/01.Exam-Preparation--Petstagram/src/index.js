bc6ece40ac0cea6d86bdd64251440997432b2b40
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-06-13 14:34:04 +0300

    Install and config handlebars.

--- JS_Back-End/Exams/01.Exam-Preparation--Petstagram/src/index.js
+++ JS_Back-End/Exams/01.Exam-Preparation--Petstagram/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
 const routes = require('./routes');
 
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 const app = express();
 
 expressConfig(app);
+handlebarsConfig(app);
 
 app.use(routes);
 
