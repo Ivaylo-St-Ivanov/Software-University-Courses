5bf2f20ad43d39c574fc7147adb953220eb527fe
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-10-14 14:06:01 +0300

    Added tasks solutions to "Regular_Exam_14_October_2023" in PostgreSQL course.

---
+++ PostgreSQL/Exams/04.Regular_Exam_14_October_2023/4.9.stadium_teams_information.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION fn_stadium_team_name(
+	stadium_name VARCHAR(30)
+) RETURNS TABLE ("name" VARCHAR(30)) AS
+$$
+BEGIN
+    RETURN QUERY (
+	    SELECT
+		    t.name
+		FROM
+		    teams AS t
+		JOIN
+		    stadiums AS s
+		ON
+		    s.id = t.stadium_id
+		WHERE
+		    s.name = stadium_name
+		ORDER BY
+		    t.name ASC
+	);
+END
+$$
+LANGUAGE plpgsql;
